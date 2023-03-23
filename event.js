6c592fb71bfa6aaa87d269fef963f74736562681
Desperado <113103241+desperadoNikita@users.noreply.github.com>
2023-03-23 21:02:12 +0200

    Add files via upload

---
+++ event.js
@@ -0,0 +1,27 @@
+ 
+ const form = document.getElementById("loginForm");
+ const usernameInput = document.getElementById("username");
+ const passwordInput = document.getElementById("password");
+
+ form.addEventListener("submit", (event) => {
+    event.prevent();
+
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+
+    if (username === "" || password === "") {
+      console.log("Заповніть поля");
+      return;
+    }
+
+    console.log(`Username: ${username}, Password: ${password}`);
+    form.reset();
+  });
+
+  usernameInput.addEventListener("input", (event) => {
+    console.log(`Введені символи: ${event.target.value}`);
+  });
+
+  passwordInput.addEventListener("input", (event) => {
+    console.log(`Введені символи: ${event.target.value}`);
+  });
